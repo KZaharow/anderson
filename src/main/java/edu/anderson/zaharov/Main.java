4fdc7833cc57ebe7411f593b9f222f67b3e9b8c7
zaharov <KZaharow@gmail.com>
2021-07-12 15:38:03 +0300

    update README.md

--- src/main/java/edu/anderson/zaharov/Main.java
+++ src/main/java/edu/anderson/zaharov/Main.java
@@ -1,12 +1,19 @@
 package edu.anderson.zaharov;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) throws NullPointerException {
 
         try {
-            throw new NullPointerException();
-        } catch (Error e){
+            Files.readAllLines(Paths.get("Test.txt"));
+        } catch (NoSuchFileException | IOException e) {
+            e.printStackTrace();
         }
     }
 }
