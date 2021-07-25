fdf17fea9a45b0b72ac4121b855d2a6efdcad873
zaharov <KZaharow@gmail.com>
2021-07-25 20:36:02 +0300

    clean code finish

--- src/main/java/edu/anderson/zaharov/exception/CustomArrayException.java
+++
@@ -1,9 +0,0 @@
-package edu.anderson.zaharov.exception;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class CustomArrayException extends Exception {
-
-    private final String msg;
-}
