bc893d5a95b36de054cec52adf57536678b4d649
zaharov <KZaharow@gmail.com>
2021-07-29 12:35:30 +0300

    change AppCfg

--- src/main/java/edu/anderson/zaharov/spring_annotation/Main.java
+++ src/main/java/edu/anderson/zaharov/spring_annotation/Main.java
@@ -1,11 +1,47 @@
 package edu.anderson.zaharov.spring_annotation;
 
+import edu.anderson.zaharov.spring_annotation.config.AppCfg;
+import edu.anderson.zaharov.spring_annotation.yaml.YamlReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.scan("by.kit.test");
+
+    public static void main(String... args) {
+
+        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(AppCfg.class);
+        int i = ctx.getBeanDefinitionCount();
+        System.out.println(i);
+        ctx.close();
+/*
+        SingerDao singerDao = ctx.getBean(SingerDao.class);
+        Singer singer = singerDao.findById(2l);
+        logger.info(singer.toString());
+
+        singerDao.delete(singer);
+
+        listSingersWithAlbum(singerDao.findAllWithAlbum());
+        ctx.close();*/
     }
+
+    /*private static void listSingersWithAlbum(List<Singer> singers) {
+        logger.info(" ---- Listing singers with instruments:");
+        singers.forEach(s -> {
+            logger.info(s.toString());
+            if (s.getAlbums() != null) {
+                s.getAlbums().forEach(a -> logger.info("\t" + a.toString()));
+            }
+            if (s.getInstruments() != null) {
+                s.getInstruments().forEach(i -> logger.info("\tInstrument: " + i.getInstrumentId()));
+            }
+        });
+    }*/
 }
--- src/main/java/edu/anderson/zaharov/spring_annotation/config/AppCfg.java
+++ src/main/java/edu/anderson/zaharov/spring_annotation/config/AppCfg.java
@@ -1,98 +1,71 @@
 package edu.anderson.zaharov.spring_annotation.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement(proxyTargetClass = true)
-@ComponentScan(basePackages = {"edu.anderson.zaharov.spring_annotation.*"})
-@EnableJpaRepositories(basePackages = {"edu.anderson.zaharov.spring_annotation.*"})
+@ComponentScan(basePackages = "edu.anderson.zaharov.spring_annotation")
+@EnableTransactionManagement
 @Slf4j
-public class AppCfg implements WebMvcConfigurer {
+public class AppCfg {
 
 
     @Bean
     public DataSource dataSource() {
 
         try {
-            log.info("dataSource bean loading");
             EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
-            return dbBuilder.setType(EmbeddedDatabaseType.H2)
-                    .addScripts("classpath:sql/schema.sql", "classpath:sql/test-data.sql").build();
+            return dbBuilder.
+                    setType(EmbeddedDatabaseType.H2).
+                    setName("myTest").
+                    addScripts("classpath:sql/init.sql").
+                    build();
         } catch (Exception e) {
-            log.error("dataSource bean error, {}", e.getMessage());
+            log.error("Embedded DataSource bean cannot be created!", e);
             return null;
         }
     }
 
-    /*@Bean
-    public PlatformTransactionManager transactionManager() {
-
-        try {
-            System.out.println("transactionManager");
-            return new JpaTransactionManager(entityManagerFactory());
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + " " + "transactionManager");
-            return null;
-        }
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        try {
-            System.out.println("jpaVendorAdapter");
-            return new HibernateJpaVendorAdapter();
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + " " + "jpaVendorAdapter");
-            return null;
-        }
+    private Properties hibernateProperties() {
+        
+        Properties hibernateProp = new Properties();
+        hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProp.put("hibernate.format_sql", true);
+        hibernateProp.put("hibernate.use_sql_comments", true);
+        hibernateProp.put("hibernate.show_sql", true);
+        hibernateProp.put("hibernate.max_fetch_depth", 3);
+        hibernateProp.put("hibernate.jdbc.batch_size", 10);
+        hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+        return hibernateProp;
     }
 
     @Bean
-    public Properties hibernateProperties() {
+    public SessionFactory sessionFactory() throws IOException {
 
-        try {
-            System.out.println("hibernateProperties");
-            Properties hibernateProp = new Properties();
-            hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-            hibernateProp.put("hibernate.format_sql", true);
-            hibernateProp.put("hibernate.use_sql_comments", true);
-            //hibernateProp.put("hibernate.show_sql", true);
-            hibernateProp.put("hibernate.max_fetch_depth", 3);
-            hibernateProp.put("hibernate.jdbc.batch_size", 10);
-            hibernateProp.put("hibernate.jdbc.fetch_size", 50);
-            return hibernateProp;
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + " " + "hibernateProperties");
-            return null;
-        }
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("edu.anderson.zaharov.spring_annotation.entity");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() {
+    public PlatformTransactionManager transactionManager() throws IOException {
 
-        try {
-            System.out.println("entityManagerFactory");
-            LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-            factoryBean.setPackagesToScan("by.kit.test");
-            factoryBean.setDataSource(dataSource());
-            factoryBean.setJpaProperties(hibernateProperties());
-            factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
-            factoryBean.afterPropertiesSet();
-            return factoryBean.getNativeEntityManagerFactory();
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + " " + "entityManagerFactory");
-            return null;
-        }
-    }*/
+        return new HibernateTransactionManager(sessionFactory());
+    }
 }
